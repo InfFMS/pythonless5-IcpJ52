2bf983d9cdad75f6513ba0d2acfd0ee48d1a0c77
IcpJ42 <iblion08@gmail.com>
2024-10-17 17:13:21 +0700

    Переделал в соответствии с PEP-8

--- task10.py
+++ task10.py
@@ -7,12 +7,13 @@
 # Угол = 90
 from random import randint
 from math import acos, pi
+
 n = int(input())
 a = [randint(-100, 100) for _ in range(n)]
 b = [randint(-100, 100) for _ in range(n)]
 scalar = sum([a[i] * b[i] for i in range(n)])
-len_a = (sum([x**2 for x in a]))**0.5
-len_b = (sum([x**2 for x in b]))**0.5
+len_a = (sum([x ** 2 for x in a])) ** 0.5
+len_b = (sum([x ** 2 for x in b])) ** 0.5
 angle = acos(scalar / (len_a * len_b)) * 180 / pi
 print('Вектор a:', a)
 print('Вектор b:', b)
