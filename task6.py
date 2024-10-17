53acb21191b95421361022923f7e85199a62196e
IcpJ42 <iblion08@gmail.com>
2024-10-17 17:53:42 +0700

    Сделал 6 задание

--- task6.py
+++ task6.py
@@ -4,3 +4,10 @@
 # Пример: ввод N = 6
 # [1,2,3,4,5,6]
 # Вывод: [3,2,1,6,5,4]
+from random import randint
+
+n = int(input())
+lst = [randint(-100, 100) for _ in range(n)]
+print(lst)
+lst = lst[n // 2 - 1::-1] + lst[:n // 2 - 1:-1]
+print(lst)
