31fb407e482019c026d101661af63908a1e54c7d
IcpJ42 <iblion08@gmail.com>
2024-10-21 13:28:41 +0700

    Сделал 4 задание

--- task4.py
+++ task4.py
@@ -3,3 +3,15 @@
 # Сымитруем данные списком длины N со случайными числами в диапазоне от 0 до 1000
 # Удалите из этого списка все значения, которые на 30 % отличаются
 # от среднего значения списка
+from random import randint
+
+n = int(input())
+lst = [randint(0, 1000) for _ in range(n)]
+print(lst)
+mean = sum(lst) / n
+try:
+    lst.remove(1.3 * mean)
+    lst.remove(0.7 * mean)
+except ValueError:
+    pass
+print(lst)
